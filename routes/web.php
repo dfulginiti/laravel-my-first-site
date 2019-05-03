eaf7c8e643d4acdad738a2775f96e765df02ae44
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-03 13:33:38 -0400

    Initial

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
