66e234d76904987c20439d28b574c155f1beb419
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-04 09:16:54 -0400

    Use __call() to handle static actions

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,16 +14,20 @@ class PagesController extends Controller
             'Pick up food for later'
         ];
 
-        return view('welcome')->withTasks($tasks);
+        return view('home')->withTasks($tasks);
     }
 
-    public function about()
+    /**
+     * Instead of hard coding each controller action we can dynamically render the appropriate
+     * view and pass through any given arguments
+     *
+     * @param string $method
+     * @param array $args
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View|mixed
+     */
+    public function __call($method, $args)
     {
-        return view('about');
-    }
-
-    public function contact()
-    {
-        return view('contact');
+        return view($method)->with($args);
     }
 }
