f70776ce88aef5df10650661941d16f6dbdee4df
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-05 21:54:28 -0400

    Import bulma cdn and wrap main content in container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css">
     <title>@yield('title', 'My first app')</title>
 </head>
 <body>
@@ -14,6 +14,8 @@
         <li><a href="/contact">Contact</a></li>
     </ul>
 
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
