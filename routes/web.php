3055a6089099bf2e22bd81ed5c501740a1a80baa
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-05 20:01:11 -0400

    Create project controller and index action / route

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
+Route::get('/projects', 'ProjectController@index');
