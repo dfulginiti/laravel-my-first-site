d582fef6a3c393b523fb351c3b7b44b49b0dfe19
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-10 07:35:58 -0400

    Add association to tasks

--- app/Project.php
+++ app/Project.php
@@ -11,4 +11,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
