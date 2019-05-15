18549a63ef39e8c4f5dc287bed7f2729ab20bf04
Derek Fulginiti <derek.fulginiti@gmail.com>
2019-05-14 21:43:37 -0400

    Refactor

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,21 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Projects</title>
-</head>
-<body>
-    <h1>Projects</h1>
+@extends('layouts.app')
 
-    @foreach($projects as $project)
-        <li>
-            <a href="/projects/{{ $project->id }}">
-                {{ $project->title }}
-            </a>
-        </li>
-    @endforeach
-</body>
-</html>
+@section('content')
+    <h1 class="title">Projects</h1>
+
+    <ul>
+        @foreach($projects as $project)
+                <a href="/projects/{{ $project->id }}">
+                    <li>{{ $project->title }}</li>
+                </a>
+        @endforeach
+    </ul>
+@endsection
